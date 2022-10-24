22fddd1bf5a10198ec8572b3b3cd2c722fc7a320
valdirnt <valdirnt@gmail.com>
2022-10-24 17:05:25 -0300

    Tiro funcionando, com vida do inimigo e dano do tiro, mira implementada, partícula de fire rate adicionado, morte de inimigo

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,6 +4,9 @@ public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    
+    public GameObject impactEffect;
+    public float impactForce = 30f;
 
     Transform cam;
 
@@ -19,6 +22,20 @@ public class Gun : MonoBehaviour
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
         {
             Debug.Log(hitInfo.transform.name);
+
+            Target target = hitInfo.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hitInfo.rigidbody != null)
+            {
+                hitInfo.rigidbody.AddForce(hitInfo.normal * (- impactForce));
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+            Destroy(impactGO, 2f);
         }
 
     }
